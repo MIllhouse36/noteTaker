53fd87227e996e945b55433d7dea57550b5a2904
Kevin <millhousekevin@gmail.com>
2020-08-07 20:47:59 -0400

    delete works! right!

---
+++ routes/apiroutes.js
@@ -0,0 +1,65 @@
+const path = require("path");
+const OUTPUT_DIR = path.resolve(__dirname, "../db");
+const outputPath = path.join(OUTPUT_DIR, "db.json");
+const fs = require("fs");
+const express = require("express");
+const router = express.Router();
+
+console.log({outputPath, OUTPUT_DIR});
+// Api Routes =============================================================
+
+router.get("/api/notes", function (req, res) {
+  fs.readFile(outputPath, "utf8", (err, data) => {
+    if (err) throw err;
+    const dbJson = JSON.parse(data);
+    // console.log(dbJson);
+    return res.json(dbJson);
+  });
+});
+
+router.post("/api/notes", function (req, res) {
+  fs.readFile(outputPath, "utf8", (err, data) => {
+    if (err) throw err;
+    const dbJson = JSON.parse(data);
+    console.log(dbJson);
+    console.log(dbJson === []);
+    let newID;
+    if (dbJson.length > 0) {
+      newID = dbJson[dbJson.length - 1].id + 1;
+      console.log(newID)
+    }else{
+      newID = 0;
+    }
+    let newNote = req.body;
+    newNote.id = newID;
+    // const newNote = { id: newID, ...req.body };
+    dbJson.push(newNote);
+    fs.writeFile(outputPath, JSON.stringify(dbJson, null, 2, +","), (err) => {
+      if (err) throw err;
+      return res.json(newNote);
+    })
+  });
+});
+
+router.delete("/api/notes/:id", function(req, res) {
+  const selectedNote = req.params.id;
+  console.log(selectedNote);
+  // const chosen = JSON.parse(req.params.id); 
+  //logs chosen number
+  // console.log(chosen);
+  fs.readFile(outputPath, "utf8", (err, data) => {
+    if (err) throw err;
+    let pData = JSON.parse(data);
+      pData = pData.filter(function(pnotes){
+        return pnotes.id != req.params.id;
+      })
+
+    fs.writeFile(outputPath, JSON.stringify(pData, null, 2, +","), (err)=> {
+      if (err) throw err;
+      res.json(selectedNote);
+  });
+      
+  });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 const express = require("express");
 const app = express();
-const fs = require("fs");
-const path = require("path");
-const OUTPUT_DIR = path.resolve(__dirname, "db");
-const outputPath = path.join(OUTPUT_DIR, "db.json");
+
+
 const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
@@ -13,69 +11,12 @@ app.use(express.static("public"));
 //research spread operator!!
 // Api Routes =============================================================
 
-app.get("/api/notes", function (req, res) {
-  fs.readFile(outputPath, "utf8", (err, data) => {
-    if (err) throw err;
-    const dbJson = JSON.parse(data);
-    // console.log(dbJson);
-    return res.json(dbJson);
-  });
-});
-
-app.post("/api/notes", function (req, res) {
-  fs.readFile(outputPath, "utf8", (err, data) => {
-    if (err) throw err;
-    const dbJson = JSON.parse(data);
-    console.log(dbJson);
-    console.log(dbJson === []);
-    let newID;
-    if (dbJson.length > 0) {
-      newID = dbJson[dbJson.length - 1].id + 1;
-      console.log(newID)
-    }else{
-      newID = 0;
-    }
-    let newNote = req.body;
-    newNote.id = newID;
-    // const newNote = { id: newID, ...req.body };
-    dbJson.push(newNote);
-    fs.writeFile(outputPath, JSON.stringify(dbJson, null, 2, +","), (err) => {
-      if (err) throw err;
-      return res.json(newNote);
-    })
-  });
-});
-
-app.delete("/api/notes/:id", function(req, res) {
-  const selectedNote = req.params.id;
-  console.log(selectedNote);
-  // const chosen = JSON.parse(req.params.id); 
-  //logs chosen number
-  // console.log(chosen);
-  fs.readFile(outputPath, "utf8", (err, data) => {
-    if (err) throw err;
-    let pData = JSON.parse(data);
-      pData = pData.filter(function(pnotes){
-        return pnotes.id != req.params.id;
-      })
-
-    fs.writeFile(outputPath, JSON.stringify(pData, null, 2, +","), (err)=> {
-      if (err) throw err;
-      res.json(selectedNote);
-  });
-      
-  });
-});
+const apiroutes = require("./routes/apiroutes")
+app.use(apiroutes);
 
 // htmlRoutes =============================================================
-app.get("/notes", function (req, res) {
-  res.sendFile(path.join(__dirname, "./public/notes.html"));
-});
-
-app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "./public/index.html"));
-});
-
+ const htmlroutes = require("./routes/htmlroutes");
+ app.use(htmlroutes);
 // Listening =============================================================
 app.listen(PORT, function () {
   consoleLOG(`App listening on PORT: ${PORT}`);
